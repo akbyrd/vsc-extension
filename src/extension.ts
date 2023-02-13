a54b435697925b37709eafb61bbc5e1bf1e3e3a7
Adam Byrd <adam1.byrd@gmail.com>
2023-02-13 15:22:38 -0800

    Initial commit - contains a few commands - akbyrd.editor.scrollTo.cursor - akbyrd.editor.cursorMoveTo.blankLine.prev.center - akbyrd.editor.cursorMoveTo.blankLine.next.center - akbyrd.editor.cursorSelectTo.blankLine.prev.center - akbyrd.editor.cursorSelectTo.blankLine.next.center

---
+++ src/extension.ts
@@ -0,0 +1,62 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext)
+{
+	let disposable : vscode.Disposable;
+
+	disposable = vscode.commands.registerTextEditorCommand("akbyrd.editor.scrollTo.cursor",
+		(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) =>
+		{
+			centerCursor(textEditor);
+		});
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerTextEditorCommand("akbyrd.editor.cursorMoveTo.blankLine.prev.center",
+		async (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) =>
+		{
+			await vscode.commands.executeCommand("cursorMove", { "to": "prevBlankLine", "by": "wrappedLine" });
+			centerCursor(textEditor);
+		});
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerTextEditorCommand("akbyrd.editor.cursorMoveTo.blankLine.next.center",
+		async (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) =>
+		{
+			await vscode.commands.executeCommand("cursorMove", { "to": "nextBlankLine", "by": "wrappedLine" });
+			centerCursor(textEditor);
+		});
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerTextEditorCommand("akbyrd.editor.cursorSelectTo.blankLine.prev.center",
+		async (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) =>
+		{
+			await vscode.commands.executeCommand("cursorMove", { "to": "prevBlankLine", "by": "wrappedLine", "select": true });
+			centerCursor(textEditor);
+		});
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerTextEditorCommand("akbyrd.editor.cursorSelectTo.blankLine.next.center",
+		async (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) =>
+		{
+			await vscode.commands.executeCommand("cursorMove", { "to": "nextBlankLine", "by": "wrappedLine", "select": true });
+			centerCursor(textEditor);
+		});
+	context.subscriptions.push(disposable);
+}
+
+function arraysEqual(a1 : readonly any[], a2 : readonly any[]) : boolean
+{
+	let equal = true;
+	equal = equal && a1.length === a2.length;
+	equal = equal && a1.every((element, index) => element === a2[index]);
+	return equal;
+}
+
+function centerCursor(textEditor: vscode.TextEditor)
+{
+	const cursorPos = textEditor.selection.active;
+	const destRange = new vscode.Range(cursorPos, cursorPos);
+	textEditor.revealRange(destRange, vscode.TextEditorRevealType.InCenter);
+}
+
+// TODO: Show friendly name for commands in keybindings GUI
